7778ebe374464eac7f2634d0abca35786012ca51
slim0007 <63984766+slim0007@users.noreply.github.com>
2022-05-06 20:47:49 +1000

    Add image

--- src/pages/Portfolio.vue
+++ src/pages/Portfolio.vue
@@ -1,3 +1,32 @@
 <template>
-  <div>portfolio</div>
+
+
+<div class = "Project">
+<img clss = "img1" v-bind:src= "melbourneImg" alt="">
+
+</div>
+
+
 </template>
+
+<script>
+const melbourneCity = require("@/assets/images/melbourne.jpg")
+
+export default {
+  name: "Portfolio",
+  data() {
+    return {
+      melbourneImg : "https://images.unsplash.com/photo-1545044846-351ba102b6d5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80",
+
+    }
+  }
+}
+</script>
+
+<style>
+.img1{
+  width: 1000px;
+}
+
+
+</style>
