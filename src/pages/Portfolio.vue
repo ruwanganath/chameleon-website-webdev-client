91a0bb215a76978ffa81071eee07fedbd6bd5b7e
slim0007 <63984766+slim0007@users.noreply.github.com>
2022-05-14 19:37:50 +1000

    initial portfolio page

--- src/pages/Portfolio.vue
+++ src/pages/Portfolio.vue
@@ -1,22 +1,54 @@
 <template>
+<div class="main">
 
 
-<div class = "Project">
-<img clss = "img1" v-bind:src= "melbourneImg" alt="">
+  <div class="fir" >
+    <img :src= "melbourneCity" alt="melboure" />
+    <div class="bottom-left">
+      <h1>SMART CITIES OPEN DATA</h1>
+    </div>
+    <div class="read-more">
+      <button type="read-more" name="button">Read More</button>
+    </div>
+  </div>
+<div/>
 
+<div class="fir" >
+  <img :src= "ev" alt="ev charging" />
+  <div class="bottom-right" style = "right: 5px" >
+    <h1>EV CHARGER PLACEMENT</h1>
+  </div>
+  <div class="read-more-left">
+    <button type="read-more" name="button">Read More</button>
+  </div>
 </div>
+<div/>
+
+<div class="fir" >
+  <img :src= "suburb" alt="melboure" />
+  <div class="bottom-left">
+    <h1>LOCATE A SOCKET</h1>
+  </div>
+  <div class="read-more">
+    <button type="read-more" name="button">Read More</button>
+  </div>
+</div>
+<div/>
 
 
+</div/>
 </template>
 
 <script>
-const melbourneCity = require("@/assets/images/melbourne.jpg")
+// const melbourneCity = require("@/assets/images/melbourne.jpg")
 
 export default {
   name: "Portfolio",
   data() {
     return {
-      melbourneImg : "https://images.unsplash.com/photo-1545044846-351ba102b6d5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80",
+      melbourneCity:require("@/assets/images/melbourne.jpg"),
+      ev:require("@/assets/images/ev.jpg"),
+      suburb:require("@/assets/images/suburb.jpg")
 
     }
   }
@@ -24,8 +56,76 @@ export default {
 </script>
 
 <style>
-.img1{
-  width: 1000px;
+.fir{
+position: relative;
+text-align:left;
+color: white;
+}
+img {
+  width: 100%;
+  height: 900px;
+}
+.bottom-left {
+  width: 40%;
+  position: absolute;
+  bottom: 180px;
+  left: 12px;
+}
+.bottom-left h1{
+  font-size : 120px;
+  font-weight: bold;
+  text-shadow: 2px 2px black;
+}
+.read-more{
+position: absolute;
+bottom:150px;
+left: 12px;
+}
+
+.read-more button{
+  background-color: transparent;
+  color: white;
+  border: none;
+  font-size:25px;
+}
+
+.read-more button:hover{
+  text-decoration: underline;
+  color: white;
+}
+
+.bottom-right {
+  width: 40%;
+  position: absolute;
+  bottom: 180px;
+  right: 12px;
+  text-align:right;
+}
+
+.bottom-right h1{
+  font-size : 120px;
+  font-weight: bold;
+  text-shadow: 2px 2px black;
+}
+
+.read-more-left{
+position: absolute;
+bottom:150px;
+right: 10px;
+}
+
+.read-more-left button{
+  background-color: transparent;
+  color: white;
+  border: none;
+  /* font-weight: bold; */
+  /* text-decoration: underline; */
+  font-size:25px;
+}
+
+.read-more-left button:hover{
+  text-decoration: underline;
+  color: white;
 }
 
 
